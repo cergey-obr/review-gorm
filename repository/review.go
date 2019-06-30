47689d7d86c9cc80c1bda8fd6da49995ad29f5cd
Sergey Obraztsov <cergey251@yandex.ru>
2019-06-30 13:05:47 +0400

    add order and where for product ids

--- repository/review.go
+++ repository/review.go
@@ -23,6 +23,7 @@ func GetAllReviews(website int, limit int, offset int) []models.Review {
 		Where("website = ?", website).
 		Limit(limit).
 		Offset(offset).
+		Order("id").
 		Find(&reviews)
 
 	return reviews
@@ -42,9 +43,11 @@ func GetProductReviews(website int, productIds []string, limit int, offset int) 
 	db.
 		Preload("Author").
 		Preload("Photos").
+		Where("product_id IN (?)", productIds).
 		Where("website = ?", website).
 		Limit(limit).
 		Offset(offset).
+		Order("id").
 		Find(&reviews)
 
 	var productReviews models.ProductReviews
