e11aada618b940bdbd8f270dc0e148229f310dcf
Sergey Obraztsov <cergey.obr@gmail.com>
2019-07-05 20:42:10 +0400

    change databse connection for office

--- repository/db.go
+++ repository/db.go
@@ -10,7 +10,7 @@ var db *gorm.DB
 func InitDatabase() {
 	var err error
 
-	db, err = gorm.Open("mysql", "go:123456@tcp(localhost:3306)/review")
+	db, err = gorm.Open("mysql", "root:123456@tcp(localhost:3307)/review")
 	if err != nil {
 		panic(err.Error())
 	}
